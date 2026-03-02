a111fee648ebd55524a4136ec738a8281296fe49
tragic-cmd <codyshouey@gmail.com>
2026-03-02 18:40:22 -0500

    D: Added the catch command to allow users to attempt to catch found pokemon

---
+++ command_catch.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"errors"
+	"math/rand"
+)
+
+func commandCatch(cfg *config, args ...string) error {
+	if len(args) != 1 {
+        return errors.New("No pokemon to catch.")
+    }
+
+	pokemonName := args[0]
+	fmt.Printf("Throwing a Pokeball at %s...\n", pokemonName)
+
+	pokemonResp, err := cfg.pokeapiClient.GetPokemon(pokemonName)
+	if err != nil {
+		return err
+	}
+
+	if pokemonResp.BaseExperience <= 0 {
+		fmt.Printf("%s was caught!\n", pokemonResp.Name)
+		cfg.caughtPokemon[pokemonResp.Name] = pokemonResp
+		return nil
+	}
+
+	roll := rand.Intn(pokemonResp.BaseExperience)
+
+	if roll > 40 {
+		fmt.Printf("%s escaped!\n", pokemonResp.Name)
+		return nil
+	} else {
+		fmt.Printf("%s was caught!\n", pokemonResp.Name)
+		cfg.caughtPokemon[pokemonResp.Name] = pokemonResp
+		return nil
+	}
+}
--- repl.go
+++ repl.go
@@ -13,6 +13,7 @@ type config struct {
 	pokeapiClient    pokeapi.Client
 	nextLocationsURL *string
 	prevLocationsURL *string
+	caughtPokemon    map[string]pokeapi.RespPokemon
 }
 
 func startRepl(cfg *config) {
@@ -81,6 +82,11 @@ func getCommands() map[string]cliCommand {
 			name:        "explore",
 			description: "Explore an area to find the pokemon hiding within",
 			callback:    commandExplore,
+		},
+		"catch": {
+			name:        "catch",
+			description: "Throw a pokeball to try catching a pokemon",
+			callback:    commandCatch,
 		},
 	}
 }
